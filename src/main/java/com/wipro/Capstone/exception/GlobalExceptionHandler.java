3010ea6a9346c6b8d7a2682398b0a32718b6721b
Rupak-66 <rbhattacharjee26@yahoo.com>
2022-04-06 11:20:24 +0530

    Updated Customer mapper logic

--- src/main/java/com/wipro/Capstone/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wipro/Capstone/exception/GlobalExceptionHandler.java
@@ -6,12 +6,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
+import java.time.Instant;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = ConstraintViolationException.class)
-    public ResponseEntity exceptionHandlerConstraintViolation(ConstraintViolationException e){
+    public ResponseEntity<Object> exceptionHandlerConstraintViolation(ConstraintViolationException e){
         return  ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+    }
+
+    @ExceptionHandler(value = CustomerNotFoundException.class)
+    public ResponseEntity<Object> exceptionHandlerCustomerNotFound(CustomerNotFoundException e){
+        Map<String, Object> errorMessage = new LinkedHashMap<>();
+        errorMessage.put("timestamp", Instant.now());
+        errorMessage.put("error",e.getMessage());
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 }
