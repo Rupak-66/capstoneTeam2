4191f15ec40da42aa53f3e0b8a0545cff2c82efa
Rupak-66 <rbhattacharjee26@yahoo.com>
2022-04-17 23:12:37 +0530

    Updated spring.jackson property on application.properties file

--- src/main/java/com/wipro/Capstone/CapstoneApplication.java
+++ src/main/java/com/wipro/Capstone/CapstoneApplication.java
@@ -2,10 +2,12 @@ package com.wipro.Capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableEurekaClient
 public class CapstoneApplication {
 
 	public static void main(String[] args) {
